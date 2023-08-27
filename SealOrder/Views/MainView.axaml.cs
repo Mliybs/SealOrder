78c541c8ef4e5885d896467fbdbfaf27513d4adb
Mliybs <2491988463@qq.com>
2023-08-27 13:20:43 +0800

    114

--- SealOrder/Views/MainView.axaml.cs
+++ SealOrder/Views/MainView.axaml.cs
@@ -7,11 +7,16 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
 
+    private void Know(object sender, RoutedEventArgs e)
+    {
         Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "114"));
 
         using var ouo = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "114","ouo.txt"));
 
         ouo.Write("114!");
+
+        MessageBoxManager.GetMessageBoxStandard(string.Empty, AppDomain.CurrentDomain.BaseDirectory).ShowAsync();
     }
 }
