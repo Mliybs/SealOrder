72e7b3e2562330c0a94e020384e3ffbf7864d93f
Mliybs <2491988463@qq.com>
2023-10-16 23:59:48 +0800

    ?

--- SealOrder.Android/MainActivity.cs
+++ SealOrder.Android/MainActivity.cs
@@ -98,7 +98,7 @@ public class MainActivity : AvaloniaMainActivity<App>
                 service.Notify(114514, new Notification.Builder(this, "GenshinImpact")
                     .SetContentTitle("我要玩原神！")
                     .SetContentText("我云游玩原神五年了，其实，我早就是一个原友了")
-                    .SetSmallIcon(Resource.Drawable.ic_notification)
+                    .SetSmallIcon(17301595)
                     .Build());
             }
             catch (System.Exception e)
