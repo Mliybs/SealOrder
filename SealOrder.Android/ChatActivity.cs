f87a3ca58a47d7b56981c484079899557cec048c
Mliybs <2491988463@qq.com>
2023-11-14 23:33:51 +0800

    ?

--- SealOrder.Android/ChatActivity.cs
+++ SealOrder.Android/ChatActivity.cs
@@ -1,8 +1,15 @@
 
 namespace SealOrder.Android;
 
-public class ChatActivity : AvaloniaMainActivity
+public class ChatActivity : AvaloniaMainActivity<App>
 {
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont()
+            .UseReactiveUI();
+    }
+
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
